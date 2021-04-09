ed5f9ac7d8fb4dfc0b7df7456450cabe9e83c78e
SAMARTH CHAWLA <73781310+SAMARTHCHAWLA333@users.noreply.github.com>
2021-04-09 14:27:26 +0530

    Add files via upload

---
+++ block.js
@@ -0,0 +1,38 @@
+class Block extends BaseClass {
+    constructor(x, y, width, height){
+      super(x,y,width,height);
+    }
+  };
+
+  function Score(){
+    if(this.visibility<0 && this.visibility>-105){
+      score++;
+    }
+    block1.score();
+    block2.score();
+    block3.score();
+    block4.score();
+    block5.score();
+    block6.score();
+    block7.score();
+    block8.score();
+    block9.score();
+    block10.score();
+    block11.score();
+    block12.score();
+    block13.score();
+    block14.score();
+    block15.score();
+    block16.score();
+    block17.score();
+    block18.score();
+    block19.score();
+    block20.score();
+    block21.score();
+    block22.score();
+    block23.score();
+    block24.score();
+    block25.score();
+    block26.score();
+    block27.score();
+  }
