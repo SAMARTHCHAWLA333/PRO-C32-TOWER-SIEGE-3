c7ca9f746ec81f65de1830bca52f075a8ea925cf
SAMARTH CHAWLA <73781310+SAMARTHCHAWLA333@users.noreply.github.com>
2021-04-19 10:51:34 +0530

    Update block.js

--- block.js
+++ block.js
@@ -26,13 +26,4 @@ class Block extends BaseClass {
     block16.score();
     block17.score();
     block18.score();
-    block19.score();
-    block20.score();
-    block21.score();
-    block22.score();
-    block23.score();
-    block24.score();
-    block25.score();
-    block26.score();
-    block27.score();
   }
